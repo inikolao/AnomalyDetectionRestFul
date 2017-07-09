49eca4d8ff47600925052cfb0a70afa63370666e
Ioannis N <Giannhspc@gmail.com>
2017-07-10 00:37:51 +0300

    Update teacherMan.java
    Better train model.

--- src/main/java/anomalydet/utilities/teacherMan.java
+++ src/main/java/anomalydet/utilities/teacherMan.java
@@ -71,6 +71,8 @@ public class teacherMan {
         String line;//this is a varaiable we use in file reading. Represents one line of the file.
         String [] elemval;// this is a 2D array we use in file reading. As the form of the file we accept in each is "Type","Value"
         //this array is used to separate the type and the value for each line.
+        ArrayList<String> typesK= new ArrayList();//Help lists in order to send the type in blocks
+        ArrayList<Double> valuesK= new ArrayList();//Help lists in order to send the values in blocks
         final BufferedReader br;// This is a Buffer we use to read the file with the training data.
         Datum dataTotrain=null;// Here we initialise a struct named Datum that is defined by the Jubatus framework as a form of communication with the Jubatus Server.
         int help3=0;//This is a helping variable that we use for debugging purposes.
@@ -79,14 +81,28 @@ public class teacherMan {
             br = new BufferedReader(new FileReader("trainDat/"+file));//This file must be in a folder named trainDat. Here we define a reader we use to read the file with the training data.
             while ((line = br.readLine()) != null) {//while we ane not in the end of file we keep reaging from file.
                 elemval=line.split(",");//we separate the type and the value for each line of the training file.
-                dataTotrain.addNumber(elemval[0], Double.parseDouble(elemval[1]));//We add the value and the type of the value to the struct named Datum. The form is (key,value)
-               server.client.add(dataTotrain);//we send the value for train to add it in the train model.
-              dataTotrain.numValues.clear();//we emty the struct Datum in order to send the next value.
-              dataTotrain.stringValues.clear();//we emty the struct Datum from posible string values
-              dataTotrain.binaryValues.clear();//we emty the struct Datum from posible binary values
-              //help3++;//testings |we count the file line for debuging reasons.
+                //We add the value and the type of the value to the struct named Datum. The form is (key,value)
+               typesK.add(elemval[0]);//we add the value in of the line in a list
+                valuesK.add(Double.parseDouble(elemval[1]));//we add the type of the line in list
+             help3++;//we count the file line in order to create the send block
+              if(help3==100)
+                {
+                    dataTotrain=createdomeTosent(typesK,valuesK);//we send the list of value for train to add it in the train model.
+                    typesK.clear();// we clear the help list of types for reuse
+                    valuesK.clear();// we clear the help list of values for reuse
+                    server.client.add(dataTotrain);//we send the list of value for train to add it in the train model.
+                    dataTotrain.numValues.clear();//we emty the struct Datum in order to send the next block of values.
+                    help3=0;//we reset the help value for the next block
+                    //break;
+                }
+              
             }
             br.close();//close the file reader.
+         dataTotrain=createdomeTosent(typesK,valuesK);//we send the list of value for train to add it in the train model.
+        typesK.clear();// we clear the help list of types to free memory.
+        valuesK.clear();// we clear the help list of values to free memory.
+        server.client.add(dataTotrain);//we send the final list of value for train to add it in the train model.
+        dataTotrain.numValues.clear();//we emty the struct Datum in order to free memory.
         } catch (FileNotFoundException ex) {//handling the case we can not find the selected data file
             System.out.println("File Not Found Error.");//sending a relative error message to console.->"File Not Found Error."
             System.exit(0);//terminate the program.
@@ -99,5 +115,25 @@ public class teacherMan {
     {//this is a method we can use in order to create the Jubatus comunication struct named DATUM.
          Datum datum = new Datum();//We define a new Jubatus comunication struct Datum         
          return datum.addNumber(key, value);//And we return it.
-    }    
+    }   
+ private static Datum createdomeTosent(ArrayList<String> key,ArrayList<Double> value)
+    {//this is a method we can use in order to create the Jubatus comunication struct named DATUM of a list of values.
+         Datum datum = new Datum();//We define a new Jubatus comunication struct Datum         
+         try{
+         for(int i=0;i<key.size();i++)
+         {
+             datum.addNumber(key.get(i), value.get(i));//we start to creating a DATUM LIST
+         
+         }
+         key.clear();
+         value.clear();
+         }
+         catch(Exception ex)
+         {
+             System.out.println("Error durring making dome to send.");//sending a relative error message to console in case of error.->"Error durring making dome to send."
+             ex.printStackTrace();
+             return null;
+         }
+         return datum;//return the List DATUM
+    }
 }
